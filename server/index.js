8fe286fe04d3623b6eb7475906fe0cfb70fc9f05
Wojtek Dasiukiewicz <wojtek@octocode.pl>
2020-05-03 22:34:29 +0200

    Random color function and return value on request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 // Nasz kod serwisu w node.js
 const express = require('express');
+const randomColor = require('./randomColor');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(randomColor());
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- server/randomColor.js
+++ server/randomColor.js
@@ -1,4 +1,4 @@
-export default () => {
+module.exports = function randomColor() {
     const colorArray = [
         '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
         '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
