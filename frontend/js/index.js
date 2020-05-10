7e080edd33956998abb0c76debdc3c946c0541e7
Wojtek Dasiukiewicz <wojtek@octocode.pl>
2020-05-10 22:32:47 +0200

    Fetch with resolved cors problem

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,9 @@
 // Frontend service script
-fetch('http://localhost:3000').then(response => {
+fetch('http://localhost:3000', {
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    headers: {
+      'Content-Type': 'application/json'
+    }
+}).then(response => {
     console.log(response);
 });
